ba3fc60e0b692d6c91e1916c550c967e38dcc922
Nieves Aracil Cerdan <nievesaracil@gmail.com>
2020-03-09 08:24:56 +0100

    Un Digito

--- src/ASumar.java
+++ src/ASumar.java
@@ -1,4 +1,24 @@
 
 public class ASumar {
-
+	private String valor_inicial;
+	
+	/**
+	 * Constructor de la clase pasandole solo un digito por parámetro.
+	 * @param string número de un dígito
+	 */
+    public ASumar(String string) {
+        this.valor_inicial = string;
+    }
+    
+    /**
+     * Muestra el resultado de la operación, dependiendo de si es un número solo, dos o el número es negativo.
+     * @return
+     */
+    public String mostrar() {
+        String numero = valor_inicial;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+        return null;
+    }
 }
